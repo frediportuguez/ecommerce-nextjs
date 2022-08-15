c01f2fc0c46430ced2ee58e12cec3ec1ad0a7c98
frediportuguez <deceitfulclimber@gmail.com>
2022-08-15 19:26:12 -0300

    layout and footerbanner finished

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head'; // is the same thing as head is in HTML
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className='layout'>
+      <Head>
+        <title>Primavera Sound</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
