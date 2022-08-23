548e02634c392aa6cf22578c7b7465fa7060d751
frediportuguez <deceitfulclimber@gmail.com>
2022-08-22 23:56:34 -0300

    implementing payment with stripe

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -11,6 +11,10 @@ const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext();
 
+  const handleCheckout = () => {
+    
+  }
+
   return (
     <div className='cart-wrapper' ref={cartRef}>
       <div className='cart-container'>
@@ -76,7 +80,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className='btn-container'>
-              <button type='button' className='btn' onClick=''>
+              <button type='button' className='btn' onClick={handleCheckout}>
                 Pay with Stripe
               </button>
             </div>
